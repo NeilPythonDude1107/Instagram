4d350d0771aaeae17803f72b52067fb46f1568e3
NeilPythonDude1107 <68854595+NeilPythonDude1107@users.noreply.github.com>
2020-10-30 15:48:40 +0530

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
     JWT_SECRET:'NeilIsTheBest',
     MONGO_URI:'mongodb+srv://Admin:Admin@instagram.8kiop.mongodb.net/Data?retryWrites=true&w=majority',
-    EMAIL:'theoriginalboommakers@gmail.com',
-    SENDGRID_API:"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",
+    EMAIL:'',
+    SENDGRID_API:"",
     CLIENT_URL:'http://localhost:3000'
 }
